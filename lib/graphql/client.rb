f1ff64f9faf893139e6e649a92701218da8e2a93
JustSnow <justsnow17@yandex.ru>
2026-05-13 12:14:32 +0300

    Start rate limmiter

--- lib/graphql/client.rb
+++ lib/graphql/client.rb
@@ -11,6 +11,26 @@ module GraphQL
     class Error < StandardError; end
 
     class Custom
+      class << self
+        attr_reader :limiter_opts
+        attr_reader :cache_opts
+
+        def rate_limiter(**opts)
+          @limiter_opts = {
+            enabled: opts.fetch(:enabled, false),
+            limit: opts.fetch(:limit, 100),
+            period: opts.fetch(:period, 1.hour.to_i)
+          }.freeze
+        end
+
+        def cache(**opts)
+          @cache_opts = {
+            enabled: opts.fetch(:enabled, false),
+            ttl: opts.fetch(:ttl, 1.hour.to_i)
+          }.freeze
+        end
+      end
+
       include Qonfig::Configurable
 
       configuration do
@@ -18,6 +38,7 @@ module GraphQL
         setting :token
         setting :access_id
         setting :url
+        setting :logger, Logger.new(STDOUT)
 
         validate 'timeout', :integer
         validate 'token', :string
@@ -29,8 +50,8 @@ module GraphQL
         @configurations = configurations.tap(&:validate!)
       end
 
-      def perform(query, variables = nil)
-        request = build_request(query, variables)
+      def perform(query, variables = nil, options = {})
+        request = build_request(query, variables, options)
         build_result(request.execute)
       rescue RestClient::ExceptionWithResponse, JSON::ParserError => error
         raise Error, "GraphQL client error (POST #{configurations.url}): #{error.message}"
@@ -40,12 +61,13 @@ module GraphQL
 
       attr_reader :configurations
 
-      def build_request(query, variables)
+      def build_request(query, variables, options)
         RestClient::Request.new(
           method: :post,
           url: configurations.url,
           payload: { query: query, variables: variables }.to_json,
-          timeout: configurations.timeout,
+          timeout: options.fetch(:timeout, configurations.timeout),
+          log: configurations.logger,
           headers: {
             'Authorization' => "Bearer #{configurations.access_id}:#{configurations.token}",
             'Content-Type' => 'application/json'
